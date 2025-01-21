1689a606ac98300099a28627e5b13c2cc8d22bdd
Oskar Sundelin <50694698+OskarSundelin@users.noreply.github.com>
2025-01-21 15:08:42 +0100

    Build basic game functionality

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 console.log("Hello World!");
+
+function playRound(humanSelection, computerSelection) {
+	
+}
+
+//Associate random number with rock, paper or scissor
+const computerSelection = getComputerChoice();
+function getComputerChoice(){
+	let input;
+
+	//Generate random number between 0-2
+	function getRandomInt() {
+		return Math.floor(Math.random() * 3);
+	}
+	//Translate random number into computer choice
+	switch(getRandomInt()){
+		case 0:
+			input = 'rock';
+			break;
+		case 1:
+			input = 'paper';
+			break;
+		case 2:
+			input = 'scissors';
+	}
+	return input;
+}
+
+//Prompt user for input, must be case-insensitive string 
+//(eg, 'rock', 'Paper', 'SCISSORS')
+const humanSelection = getHumanChoice();
+function getHumanChoice(){
+	let input = prompt("Choose rock, paper, or scissors:", "rock").toLowerCase();
+	return input;
+}
+console.log(computerSelection);
+console.log(humanSelection);
+//Compare the values of computer's choice and player's choice
+
+//Add score to winner
