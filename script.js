a810dab545f4bc7f4118319dedf7f5d8f3d0fcfc
Oskar Sundelin <50694698+OskarSundelin@users.noreply.github.com>
2025-01-21 15:40:59 +0100

    Redo structure and enable gameplay

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 console.log("Hello World!");
 
-function playRound(humanSelection, computerSelection) {
-	
-}
+
 
 //Associate random number with rock, paper or scissor
 const computerSelection = getComputerChoice();
@@ -34,8 +32,32 @@ function getHumanChoice(){
 	let input = prompt("Choose rock, paper, or scissors:", "rock").toLowerCase();
 	return input;
 }
-console.log(computerSelection);
-console.log(humanSelection);
-//Compare the values of computer's choice and player's choice
 
-//Add score to winner
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanSelection, computerSelection) {
+	//Print choices
+	let human = humanSelection;
+	console.log("Human chose " + human +"!");
+	let computer = computerSelection;
+	console.log("Computer chose " + computer +"!");
+
+	//Compare the values of computer's choice and player's choice
+	if(human == 'rock', computer == 'scissors' || human == 'paper', computer == 'rock' || human == 'scissors', computer == 'paper'){
+		console.log("Human wins!");
+		humanScore++;
+	}
+	else if (human == 'scissors', computer == 'rock' || human == 'rock', computer == 'paper' || human == 'paper', computer == 'scissors'){
+		console.log("Computer wins!");
+		computerScore++;
+	}
+	else if (human == computer){
+		console.log("Draw!");
+	}
+	else {
+		console.log("Invalid!");
+	}
+	console.log("Scores are: Computer: " + computerScore + " Human: " + humanScore);
+}
+
+playRound(humanSelection, computerSelection);
